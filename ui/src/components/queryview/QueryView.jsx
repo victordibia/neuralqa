29492088e059d1ba9947ec948fde0d49d134aef9
Victor Dibia <victor.dibia@gmail.com>
2020-05-20 00:24:23 -0700

    web interfaec 0.0

---
+++ ui/src/components/queryview/QueryView.jsx
@@ -0,0 +1,45 @@
+// Web interface to visualize example Anomaly Detection Interaction.
+// Data is visualized using the Carbon Desgin System Data Table.
+// Data is
+
+
+import React, { Component } from "react";
+import { DataTable, InlineLoading } from 'carbon-components-react';
+import { getJSONData, probabilityColor, abbreviateString, postJSONData } from "../helperfunctions/HelperFunctions"
+import "./queryview.css"
+import DetailView from "../detailview/DetailView";
+const { Table, TableHead, TableHeader, TableBody, TableCell, TableRow } = DataTable;
+
+
+class QueryView extends Component {
+    constructor(props) {
+        super(props)
+
+        this.state = {
+        }
+    }
+
+
+
+    componentDidMount() {
+    }
+
+
+    render() {
+
+        return (
+            <div>
+
+                <div className=" pt10">
+                    Welcome to Oracle
+                </div>
+
+
+
+            </div>
+
+        );
+    }
+}
+
+export default QueryView;
--- ui/src/index.js
+++ ui/src/index.js
@@ -1,15 +1,11 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
+import './components/template.scss';
+import './components/template.css';
+import App from './components/Main';
 import * as serviceWorker from './serviceWorker';
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
-);
+ReactDOM.render(<App />, document.getElementById('root'));
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
