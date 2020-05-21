8d8ade67c0cde723d378eb4e68b5188618a8a706
Victor Dibia <victor.dibia@gmail.com>
2020-05-21 02:44:04 -0700

    implemente opinion exerpt script field

--- utils/elastic_utils.py
+++ utils/elastic_utils.py
@@ -1,8 +1,10 @@
 
 import lzma
+import os
 import json
 from elasticsearch import Elasticsearch
 import logging
+from utils import data_utils
 
 
 case_index_name = "cases"
@@ -10,41 +12,38 @@ es = Elasticsearch([{'host': 'localhost', 'port': 9200}])
 es.indices.create(index=case_index_name, ignore=400)
 
 
-def get_case_law_data():
-    with lzma.open("data/mexico.jsonl.xz") as in_file:
-        for line in in_file:
-            case = json.loads(str(line, 'utf8'))
-            print(case['name'])
-            print(case.keys())
-            print(case["casebody"]["data"]["head_matter"])
-            # opinons has the bulk of the actual text
-            # print(case["casebody"]["data"].keys())
-            print(case['decision_date'])
-            break
+def create_case_index(case_file_path, max_docs=1000):
+    """Create an index from a case file (.jsonl.xz format)
 
+    Arguments:
+        case_file_path {str} -- path to case.law bulk file
 
-def create_case_index(case_file_path):
+    Keyword Arguments:
+        max_docs {int} -- maximum size of records to use in creating index.  Small default is to enable quick testing (default: {1000})
+    """
+
     i = 0
-    max_docs = 1000
+    logging.info(">> Creating index using file " + case_file_path)
     with lzma.open(case_file_path) as in_file:
         for line in in_file:
             i += 1
             case = json.loads(str(line, 'utf8'))
-            # print(case['name'])
-            # print(case.keys())
-            # print(case["casebody"]["data"]["head_matter"])
-            # # opinons has the bulk of the actual text
-            # # print(case["casebody"]["data"].keys())
-            # print(case['decision_date'])
-
             index_status = es.index(index=case_index_name, id=i, body=case)
-            print(index_status)
-
-            if (i == max_docs):
+            if (i > max_docs):
                 break
 
 
 def run_query(search_query):
+    """Makes a query to the elastic search server with the given search_query parameters
+
+
+    Arguments:
+        search_query {[dictionary]} -- [contains a dictionary that corresponds to an elastic search query on the ]
+
+    Returns:
+        [dictionary] -- [dictionary of results from elastic search.]
+    """
+    opinion_excerpt_length = 500
     search_query = {
         "_source": ["name"],
         "query": {
@@ -53,12 +52,34 @@ def run_query(search_query):
                 "fields": ["casebody.data.opinions.text", "name"]
             }
         },
-        "size": 3
+        "script_fields": {
+            "opinion_excerpt": {
+                "script": "(params['_source']['casebody']['data']['opinions'][0]['text']).substring(0," + str(opinion_excerpt_length) + ")"
+            }
+        },
+        "size": 5
     }
     query_result = es.search(index=case_index_name, body=search_query)
     # print(len(query_result["hits"]["hits"]), " hits ..")
-    print((query_result), " hits ..")
+    # for p in query_result["hits"]["hits"]:
+    #     print(len(p['_source']['casebody']['data']['opinions']), "opinions")
+    # print((query_result["hits"]["hits"]) )
     return query_result
+
+
+def setup():
+    """Called once when the webserver is instantiated
+        - Checks if the data directory is exists or is empty. If yes, downloads case law data and builds an index
+    """
+    # Check if jsonl data has been downloaded
+    if (not os.path.exists("data") or len(os.listdir("data")) == 0):
+        data_utils.get_case_data()
+        case_data_files = os.listdir("data")
+        for case_data_file in case_data_files:
+            if ("jsonl" in case_data_file):
+                create_case_index("data/" + case_data_file)
+    else:
+        logging.info(">> Data files already exist")
 
 
 # case_file_path = "data/mexico.jsonl.xz"
