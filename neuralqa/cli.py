79cdfa19ad80bdbfa237e4ed7119829f76cee9ef
Victor Dibia <victor.dibia@gmail.com>
2020-07-07 15:28:00 -0700

    config parsing logic improvement, when to load default, merging loaded config with default to ensure a welformed config at all times

--- neuralqa/cli.py
+++ neuralqa/cli.py
@@ -18,7 +18,7 @@ def cli():
 @cli_args.CONFIG_PATH
 def test(host, port, index_host, index_port, config_path):
     config = ConfigParser(config_path)
-    print(config)
+    print(config.config["reader"]["models"])
     # import_case_data()
 
 
