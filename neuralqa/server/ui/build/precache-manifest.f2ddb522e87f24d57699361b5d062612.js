22201171cc00d0d8e8404788ca535f07b8d4ed8b
Victor Dibia <victor.dibia@gmail.com>
2020-09-24 12:00:58 -0700

    add build folder to repo

---
+++ neuralqa/server/ui/build/precache-manifest.f2ddb522e87f24d57699361b5d062612.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ab09b5abfa289e05b196fbecd4c7b9b1",
+    "url": "/index.html"
+  },
+  {
+    "revision": "24ab984c723263199c47",
+    "url": "/static/css/main.0d7f6602.chunk.css"
+  },
+  {
+    "revision": "b07275a309ceb994bb56",
+    "url": "/static/js/2.17f05cd8.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/static/js/2.17f05cd8.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "24ab984c723263199c47",
+    "url": "/static/js/main.32abfeaf.chunk.js"
+  },
+  {
+    "revision": "b7d5e4d288fb48c3a4d5",
+    "url": "/static/js/runtime-main.985d1449.js"
+  }
+]);
---
+++ tests/retriever/test_retriever.py
@@ -0,0 +1,13 @@
+from neuralqa.retriever import ElasticSearchRetriever
+from neuralqa.utils import ConfigParser
+
+
+app_config = ConfigParser("test")
+print(app_config)
+
+
+def test_elasticserch_retriever():
+    print("test")
+
+
+# test_mlm_expander()
